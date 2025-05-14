1b63021394161d8548e6b632d5c55297f7b11cd9
Siva Sai Somisetty <100413125+SivaSaiSomisetty@users.noreply.github.com>
2025-05-13 19:05:56 -0500

    Update streamlit_app.py
    New major update

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
-st.title(f" :cup_with_straw: Customize Your Sai Smoothie! :cup_with_straw: ")
+st.title(f" :cup_with_straw: Customize Your Smoothie! :cup_with_straw: ")
 st.write(
   """ Choose the fruits you want in your custom Smoothie!
   """
